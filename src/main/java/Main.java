b8d8705a6e97dcdd5049e77771e50a7201a1333f
Anastasia Radachinskaia <asya070692@mail.ru>
2022-12-15 18:45:53 +0300

    test

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Before;
+
+@Before
+public void startUp() {
+        WebDriverManager.chromedriver().setup();
+}
+
+        WebDriver driver = new ChromeDriver();
+
+public class Main {
+}
